64599d30b5655e0806d37299c867cd6b1684b217
Thomas Scully <thomas1374@yahoo.com>
2015-04-06 12:35:12 -0500

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'taapp/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'taapp_controller#index'
+  root 'taapp_controller#form'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
