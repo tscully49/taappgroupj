e749bce667656088bcb0aaeb0ae23653c5605578
Ryan Cross <studiopod.ryancross@outlook.com>
2015-04-12 23:27:52 +0000

    added constraints to email field in user model: input entered, proper format, uniqueness

---
+++ test/models/user_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+  def setup 
+    @user = User.new(email: "user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email validation should accept valid address" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
+end
+
+
