9b0ac76555e991e2fd6436a64645ff616fc36ed9
Dillon J Byrne <djb8tc@mail.missouri.edu>
2015-04-13 19:05:36 +0000

    Comment out code so my changes don't restrict all access to our app before we get the login working

--- app/controllers/taapp_controller.rb
+++ app/controllers/taapp_controller.rb
@@ -1,42 +1,42 @@
 class TaappController < ApplicationController
   
   def index
-    if session[:accounttype].present?
-      if session[:accounttype] == "student"
-        redirect_to action: 'form'
-      elsif session[:accounttype] == "professor"
-        redirect_to action: 'professor'
-      elsif session[:accounttype] == "admin"
-        redirect_to action: 'admin'
-      end
-    end
+#    if session[:accounttype].present?
+#      if session[:accounttype] == "student"
+#        redirect_to action: 'form'
+#      elsif session[:accounttype] == "professor"
+#        redirect_to action: 'professor'
+#      elsif session[:accounttype] == "admin"
+#        redirect_to action: 'admin'
+#      end
+#    end
   end
 
   def form
-    if session[:accounttype].present? && session[:accounttype] == "student"
-      render "form"
-    else
-      render "formerror"
-    end
+#    if session[:accounttype].present? && session[:accounttype] == "student"
+#      render "form"
+#    else
+#      render "formerror"
+#    end
   end
 
   def register
   end
   
   def professor
-    if session[:accounttype].present? && session[:accounttype] == "professor"
-      render "professor"
-    else
-      render "proferror"
-    end
+#    if session[:accounttype].present? && session[:accounttype] == "professor"
+#      render "professor"
+#    else
+#      render "proferror"
+#    end
   end
   
   def admin
-    if session[:accounttype].present? && session[:accounttype] == "admin"
-      render "admin"
-    else
-      render "adminerror"
-    end
+#    if session[:accounttype].present? && session[:accounttype] == "admin"
+#      render "admin"
+#    else
+#      render "adminerror"
+#    end
   end
   
 end
