1edebed8861b785527b5ecafce17f26f76a910b4
Dillon J Byrne <djb8tc@mail.missouri.edu>
2015-04-14 03:15:45 +0000

    Handle log in authentication

--- app/controllers/taapp_controller.rb
+++ app/controllers/taapp_controller.rb
@@ -1,24 +1,24 @@
 class TaappController < ApplicationController
   
   def index
-#    if session[:accounttype].present?
-#      if session[:accounttype] == "student"
-#        redirect_to action: 'form'
-#      elsif session[:accounttype] == "professor"
-#        redirect_to action: 'professor'
-#      elsif session[:accounttype] == "admin"
-#        redirect_to action: 'admin'
-#      end
-#    end
+    if session[:accounttype].present?
+      if session[:accounttype] == "student"
+        redirect_to action: 'form'
+      elsif session[:accounttype] == "professor"
+        redirect_to action: 'professor'
+      elsif session[:accounttype] == "admin"
+        redirect_to action: 'admin'
+      end
+    end
   end
 
   def form
   	@application = Application.new
-#    if session[:accounttype].present? && session[:accounttype] == "student"
-#      render "form"
-#    else
-#      render "formerror"
-#    end
+    if session[:accounttype].present? && session[:accounttype] == "student"
+      render "form"
+    else
+      render "formerror"
+    end
   end
 
   def register
@@ -26,19 +26,19 @@ class TaappController < ApplicationController
   end
   
   def professor
-#    if session[:accounttype].present? && session[:accounttype] == "professor"
-#      render "professor"
-#    else
-#      render "proferror"
-#    end
+    if session[:accounttype].present? && session[:accounttype] == "professor"
+      render "professor"
+    else
+      render "proferror"
+    end
   end
   
   def admin
-#    if session[:accounttype].present? && session[:accounttype] == "admin"
-#      render "admin"
-#    else
-#      render "adminerror"
-#    end
+    if session[:accounttype].present? && session[:accounttype] == "admin"
+      render "admin"
+    else
+      render "adminerror"
+    end
   end
   
 end
