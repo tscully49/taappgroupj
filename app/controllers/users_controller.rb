fcad379310cbedbf01deb11ee3698a552606e3c0
indiahampton <iphvg5@mail.missouri.edu>
2015-04-14 02:03:25 +0000

    testing sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user=User.new(params[:user])
+    @user=User.new(user_params)
     if @user.save
       flash[:notice] = "You Signed up successfully"
       flash[:color]= "valid"
