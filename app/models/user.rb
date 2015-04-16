57a9a4778cc9bd9b62de4de9365bef0e99280c5d
Dillon J Byrne <djb8tc@mail.missouri.edu>
2015-04-16 20:07:15 +0000

    Fix email regex and display email requirements in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :first_name,  presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 50 }
   validates :student_id, presence: true, length: {maximum: 8, minimum: 8}
-  VALID_EMAIL_REGEX = /\A[\w]+@[mail]+\.[missouri]+\.[edu]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w]+@mail\.missouri\.edu\Z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
