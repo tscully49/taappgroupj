72f23cb315ffcb0546f2d10d4b6533ce8a736368
Thomas Scully <thomas1374@yahoo.com>
2015-04-20 14:27:45 -0500

    fixed errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ end
 private
 
     def user_params
-      params.require(:user).permit(:admin_name, :password, :miz_email)
+      params.require(:user).permit(:first_name, :last_name, :password, :miz_email, :password_confirmation)
     end
 end
 
