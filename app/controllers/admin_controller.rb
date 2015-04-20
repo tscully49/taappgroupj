73aa8d71fa3a6a273c2855ce2bf1e1a87ce8039d
yangwawa0202 <ylt5b@mail.missouri.edu>
2015-04-20 07:29:06 +0000

    edit professor account

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   
+  before_action :logged_in_admin, only: [:edit, :update]
   
   def show
     @admin = Admin.find(params[:id])
@@ -11,7 +12,7 @@ class AdminController < ApplicationController
   end
   
   def create
-    @admins = Professors.new(addmin_params)
+    @admins = Admin.new(addmin_params)
     if @admins.save
       log_in @admins
       flash[:success] = "Welcome to the Sample App!"
@@ -23,13 +24,33 @@ class AdminController < ApplicationController
 
 
   def edit
-    @admins = Professors.find(params[:id])
+    @admins = Admin.find(params[:id])
   end
 
-
-private
-
-    def admin_params
-      params.require(:user).permit(:admin_name, :password, :miz_email)
+  def update
+      @admins= Admin.find(params[:id])
+      if @admins.update_attributes(user_params)
+        # Handle a successful update.
+      else
+        render '/admin/edit'
+      end
     end
-end
+  
+
+  private
+  
+      def admin_params
+        params.require(:admins).permit(:admin_name, :password, :miz_email)
+      end
+      
+       def professor_params
+        params.require(:professors).permit(:prof_name, :password_digest, :miz_email)
+      end
+      
+      def logged_in_admin
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+  end
