3ef2c4d6562e7c88d6c8321bd2755077bd51385d
Thomas Scully <thomas1374@yahoo.com>
2015-04-20 19:59:23 -0500

    Added error messages to the sign up page and login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     professor = Professor.find_by(miz_email: params[:session][:email].downcase)
@@ -14,9 +16,12 @@ class SessionsController < ApplicationController
     elsif admin && admin.authenticate(params[:session][:password])
       session[:accounttype] = "admin"
       session[:id] = admin.id
+    else
+      flash.now[:notice] = 'Invalid email/password combination'
+      render "taapp/index"
     end
-    redirect_to ""
   end
+  
   def destroy
     reset_session
     redirect_to ''
