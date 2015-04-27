7776a392f9fe582e6a076bfec7abac089d03fdb4
rinarina <baorina2010@gmail.com>
2015-04-27 01:19:57 +0000

    rbync change file

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -22,23 +22,26 @@ end
 
   def edit
     @admins = Professor.find(params[:id])
-    if @admins.save
-        
-      flash[:notice] = "Professor account is edited successfully"
-      flash[:color]= "valid"
-      redirect_to '/admin/successpage'
-      redirect_to(:back)
-    else
-      render '/admin/edit'
-      redirect_to(:back)
-    end
+   # @all_profs = Professor.all
+    render "edit"
+     #if @admins.update_attributes(user_params)
+        #@admins.save
+        #flash[:notice] = "Professor account is edited successfully"
+        #flash[:color]= "valid"
+        #redirect_to(:back)
+    #else
+        #flash[:notice] = "Edit failed"
+       # redirect_to(:back)
+    #end
   end
 
   def update
       @admins= Professor.find(params[:id])
       if @admins.update_attributes(user_params)
-        redirect_to '/taapp/successpage'
+        flash[:notice] = "Professor account is updated successfully"
+        redirect_to @admins
       else
+        flash[:notice] = "Edit failed"
         render '/admin/edit'
       end
   end
