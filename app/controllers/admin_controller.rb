7d6e5cbe73c6c30f96ec95249dd0924b94e4900b
Thomas Scully <thomas1374@yahoo.com>
2015-04-28 15:38:43 -0500

    changed stuff on assigning ta's to courses

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -66,25 +66,31 @@ class AdminController < ApplicationController
 
       if @test != nil
         @open_spots_in_class = Course.find(params[:course_id])
-        if @open_spots_in_class != 0
-          params[:select_tas].each do |ta|
-            @name = Application.select(:first_name, :last_name).where(:id => ta)
-            @selection = Application.find_by(:id => ta)
-            if @selection.update(:course_id => params[:course_id])
-              @remove_app = Application.find_by(:id => ta, accepted: false)
-              @remove_app.update(accepted: true)
-              @remove_column = ApplicationCourse.where(:application_id => ta, :taught_teach_take_want => 'want')
-              @remove_column.each do |remove|
-                remove.destroy
-                @open_spots_in_class.decrement!(:open_spots)
-                if @open_spots_in_class == 0
-                  redirect_to :back
+        if @open_spots_in_class < 1
+          @number = params[:select_tas].count
+          if ((@open_spots_in_class - @number) < 0)
+            params[:select_tas].each do |ta|
+              @name = Application.select(:first_name, :last_name).where(:id => ta)
+              @selection = Application.find_by(:id => ta)
+              if @selection.update(:course_id => params[:course_id])
+                @remove_app = Application.find_by(:id => ta, accepted: false)
+                @remove_app.update(accepted: true)
+                @remove_column = ApplicationCourse.where(:application_id => ta, :taught_teach_take_want => 'want')
+                @remove_column.each do |remove|
+                  remove.destroy
+                  @open_spots_in_class.decrement!(:open_spots)
+                  if @open_spots_in_class < 1
+                    redirect_to :back
+                  end
                 end
+                flash[:notice] = "TA's Added Successfully"
+              else 
+                flash[:notice] = "TA... FIX THIS " + @name.first.first_name + "  "+@name.first.last_name+" could NOT be added"
+                #flash[:notice] = params
               end
-              flash[:notice] = "TA's Added Successfully"
-            else 
-              flash[:notice] = "TA... FIX THIS " + @name.first.first_name + "  "+@name.first.last_name+" could NOT be added"
-              #flash[:notice] = params
+            else
+              flash[:notice] = "There aren't enough open spots for all selected TA's"
+              redirect_to :back
             end
           end
           redirect_to :back
