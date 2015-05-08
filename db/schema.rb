b62cb34831afbf04bca6723ef05bd4e85137b08f
Thomas Scully <tps9tb@mail.missouri.edu>
2015-05-08 21:30:30 +0000

    THIS NEEDS TOW ROK

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508211631) do
+ActiveRecord::Schema.define(version: 20150508212816) do
 
   create_table "admins", force: true do |t|
     t.string "admin_name",      limit: 60,  null: false
