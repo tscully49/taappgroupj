1e85883a04dcf3a3a562c2bb980d0cc082a9bd6b
Thomas Scully <tps9tb@mail.missouri.edu>
2015-05-08 20:21:29 +0000

    Fixed buttons and database

--- app/models/application.rb
+++ app/models/application.rb
@@ -6,7 +6,7 @@ class Application < ActiveRecord::Base
 	before_save { self.mizzou_email = mizzou_email.downcase }
 	validates :first_name,  presence: true, length: { maximum: 50 }
 	validates :last_name,  presence: true, length: { maximum: 50 }
-	validates :student_id,  presence: true, length: { maximum: 8, minimum: 8 }
+	validates :student_id,  presence: true
 	validates :gpa,  presence: true, numericality: {less_than_or_equal_to: 4.00} #Make sure you validate that it is #.### three decimal places
 	VALID_PHONE_REGEX = /\A\d\d\d\-\d\d\d\-\d\d\d\d\z/i
 	validates :phone_num, presence: true, 
